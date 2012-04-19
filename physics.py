88659f1b2d611657d9d4fd0faa9d5ae9b2e999cd
Izaak Meckler <izaakmeckler@me.com>
2012-04-18 22:03:08 -0500

    deleted physics module

--- physics.py
+++
@@ -1,8 +0,0 @@
-# calculate coulomb's law force for the given parameters
-def coulomb(dist_vect, k, q1, q2):
-    return (k * q1 * q2 / ((dist_vect.length()+1) ** 2)) * dist_vect.normed()
-
-# calculate the hooke's law force for the given parameters
-def hooke(dist_vect, k, r):
-    rest_vect = r * dist_vect.normed()
-    return -k * (dist_vect - rest_vect)
